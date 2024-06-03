d6f85ddc4fd8c71cff61a14ad7bfe27488e0747e
shkwon98 <seonghyeon.kwon@avikus.ai>
2024-06-03 13:58:22 +0900

    Update library aliases and file paths

--- src/filesystem/register_nodes.cpp
+++ src/filesystem/register_nodes.cpp
@@ -1,12 +1,12 @@
-#include "robl_behavior_tree/filesystem/copy_file_node.h"
-#include "robl_behavior_tree/filesystem/find_file_node.h"
-#include "robl_behavior_tree/filesystem/get_file_type_node.h"
+#include "robl_behaviortree/filesystem/copy_file_node.h"
+#include "robl_behaviortree/filesystem/find_file_node.h"
+#include "robl_behaviortree/filesystem/get_file_type_node.h"
 
 #include <behaviortree_cpp/bt_factory.h>
 
 BT_REGISTER_NODES(factory)
 {
-    factory.registerNodeType<robl_behavior_tree::CopyFileNode>("CopyFile");
-    factory.registerNodeType<robl_behavior_tree::FindFileNode>("FindFile");
-    factory.registerNodeType<robl_behavior_tree::GetFileTypeNode>("GetFileType");
+    factory.registerNodeType<robl_behaviortree::CopyFileNode>("CopyFile");
+    factory.registerNodeType<robl_behaviortree::FindFileNode>("FindFile");
+    factory.registerNodeType<robl_behaviortree::GetFileTypeNode>("GetFileType");
 }
